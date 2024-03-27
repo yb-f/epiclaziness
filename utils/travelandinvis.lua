ad62ef2cd22111315c3e63c8786475929d4c00ec
Lisie <lc.x@tutanota.com>
2024-03-27 18:58:44 -0500

    Set some quests correctly

--- utils/travelandinvis.lua
+++ utils/travelandinvis.lua
@@ -2,21 +2,23 @@ local mq = require('mq')
 
 local travel = {
     ['shm_10'] = {
-        ['gate'] = 19,
+        ['gate'] = 20,
         ['invis'] = 23
     },
+    --Not accurate
     ['shm_pre15'] = {
         ['gate'] = 19,
         ['invis'] = 23
     },
     ['shm_15'] = {
-        ['gate'] = 23,
-        ['invis'] = 31
+        ['gate'] = 24,
+        ['invis'] = 32
     },
     ['shm_20'] = {
-        ['gate'] = 19,
-        ['invis'] = 23
+        ['gate'] = 16,
+        ['invis'] = 14
     },
+    --Not accurate
     ['shd_10'] = {
         ['gate'] = 69,
         ['invis'] = 88
@@ -33,6 +35,7 @@ local travel = {
         ['gate'] = 19,
         ['invis'] = 23
     },
+    --Not accurate
     ['brd_pre15'] = {
         ['gate'] = 5,
         ['invis'] = 11
@@ -42,28 +45,51 @@ local travel = {
         ['invis'] = 11
     },
     ['brd_15'] = {
-        ['gate'] = 5,
-        ['invis'] = 11
+        ['gate'] = 27,
+        ['invis'] = 33
     },
     ['brd_20'] = {
-        ['gate'] = 5,
-        ['invis'] = 11
+        ['gate'] = 4,
+        ['invis'] = 13
     },
+    --Not accurate
     ['rog_10'] = {
         ['gate'] = 5,
         ['invis'] = 11
     },
+    --Not accurate
     ['rog_15'] = {
         ['gate'] = 5,
         ['invis'] = 11
     },
+    --Not accurate
     ['rog_20'] = {
         ['gate'] = 5,
         ['invis'] = 11
     },
     ['rog_pre15'] = {
+        ['gate'] = 7,
+        ['invis'] = 8
+    },
+    --Not accurate
+    ['clr_10'] = {
         ['gate'] = 5,
         ['invis'] = 11
+    },
+    --Not accurate
+    ['clr_15'] = {
+        ['gate'] = 5,
+        ['invis'] = 11
+    },
+    --Not accurate
+    ['clr_20'] = {
+        ['gate'] = 5,
+        ['invis'] = 11
+    },
+    --Not accurate
+    ['clr_pre15'] = {
+        ['gate'] = 7,
+        ['invis'] = 8
     },
 }
 
