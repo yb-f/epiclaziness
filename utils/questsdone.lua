eafc4d2d24412b08748f680d515ef75ab9f903a1
Lisie <lc.x@tutanota.com>
2024-04-02 22:54:47 -0500

    Added File to create table of completed quests per class

---
+++ utils/questsdone.lua
@@ -0,0 +1,24 @@
+local mq = require('mq')
+
+local elheader = "\ay[\agEpic Laziness\ay]"
+
+local quests_done = {
+    ['brd'] = { "Pre-1.5", "1.5", "2.0" },
+    ['bst'] = { "Pre-1.5", "1.5", },
+    ['ber'] = {},
+    ['clr'] = { "Pre-1.5", "1.5", "2.0" },
+    ['dru'] = {},
+    ['enc'] = {},
+    ['mag'] = {},
+    ['mnk'] = {},
+    ['nec'] = {},
+    ['pal'] = {},
+    ['rng'] = {},
+    ['rog'] = { "Pre-1.5" },
+    ['shd'] = { "Pre-1.5", "1.5", "2.0" },
+    ['shm'] = { "1.0", "1.5", "2.0" },
+    ['war'] = {},
+    ['wiz'] = {},
+}
+
+return quests_done
