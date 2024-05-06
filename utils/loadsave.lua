866608d714a0cd9861c9e92c4d36ba05f8001849
Lisie <lc.x@tutanota.com>
2024-05-06 01:45:37 -0500

    fix:removed debug print statement

--- utils/loadsave.lua
+++ utils/loadsave.lua
@@ -55,7 +55,6 @@ function loadsave.loadState()
                 if _G.State:readTaskRunning() == false then
                     _G.State.epic_choice = loadsave.SaveState[class].Last_Ran
                 else
-                    print(loadsave.SaveState[class][_G.State.epic_choice].Step)
                     _G.State.current_step = loadsave.SaveState[class][_G.State.epic_choice].Step or 0
                     logger.log_info("\aoStarting on step: \ar%s\ao.", _G.State.current_step)
                 end
