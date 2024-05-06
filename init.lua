31bf01e7df1f7ba8d29cd51a441dcd3861b06326
Lisie <lc.x@tutanota.com>
2024-05-06 16:48:47 -0500

    feat:Version bump v0.3.8

--- init.lua
+++ init.lua
@@ -20,7 +20,7 @@ local http                = PackageMan.Require('luasocket', 'socket.http')
 local ssl                 = PackageMan.Require('luasec', 'ssl')
 
 local version_url         = 'https://raw.githubusercontent.com/yb-f/EL-Ver/master/latest_ver'
-local version             = v("0.3.7")
+local version             = v("0.3.8")
 local window_flags        = bit32.bor(ImGuiWindowFlags.None)
 local openGUI, drawGUI    = true, true
 local myName              = mq.TLO.Me.DisplayName()
