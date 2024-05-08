16fd38dcb75dbca2ecdb4ffc58f9721ddbf7088b
Lisie <lc.x@tutanota.com>
2024-05-08 01:11:51 -0500

    fix:generate all parameters at execution time.

--- utils/task_functions.lua
+++ utils/task_functions.lua
@@ -4,6 +4,7 @@ local inv            = require('utils/inventory')
 local travel         = require('utils/travel')
 local manage         = require('utils/manageautomation')
 local mob            = require('utils/mob')
+local logger         = require('utils/logger')
 
 class_settings.loadSettings()
 loadsave.loadState()
@@ -11,7 +12,7 @@ loadsave.loadState()
 local task_functions = {
     ADVENTURE_ENTRANCE       = {
         func   = _G.Actions.adventure_entrance,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = 'Determine which LDON Entrance to use.',
     },
     AUTO_INV                 = {
@@ -21,17 +22,17 @@ local task_functions = {
     },
     BACKSTAB                 = {
         func   = mob.backstab,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Backstab' .. item.npc end
     },
     CAST_ALT                 = {
         func   = _G.Actions.cast_alt,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Cast alt ability ' .. item.what end
     },
     COMBINE_CONTAINER        = {
         func   = inv.combine_container,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Prepare to combine items in ' .. item.what end
     },
     COMBINE_DO               = {
@@ -41,7 +42,7 @@ local task_functions = {
     },
     COMBINE_DONE             = {
         func   = inv.combine_done,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = 'Combine complete, restore item to previous bag slot.'
     },
     COMBINE_ITEM             = {
@@ -96,42 +97,42 @@ local task_functions = {
     },
     FARM_CHECK               = {
         func   = _G.Actions.farm_check,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Check if we have the items we need: ' .. item.what end
     },
     FARM_CHECK_PAUSE         = {
         func   = _G.Actions.farm_check_pause,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Check if we have ' .. item.what .. ' pause script of we do not' end
     },
     FARM_RADIUS              = {
         func   = _G.Actions.farm_radius,
-        params = { class_settings.settings, loadsave.SaveState, false },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Farm for ' .. item.what end
     },
     FARM_RADIUS_EVENT        = {
         func   = _G.Actions.farm_radius,
-        params = { class_settings.settings, loadsave.SaveState, true },
+        params = function() return { class_settings.settings, loadsave.SaveState, true } end,
         desc   = function(item) return 'Farm until event occurs. (' .. item.phrase .. ')' end
     },
     FARM_WHILE_NEAR          = {
         func   = _G.Actions.farm_while_near,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Farm until ' .. item.npc .. ' moves away.' end
     },
     FISH_FARM                = {
         func   = _G.Actions.fish_farm,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Fish for ' .. item.what end
     },
     FISH_ONCE                = {
         func   = _G.Actions.fish_farm,
-        params = { class_settings.settings, loadsave.SaveState, true },
+        params = function() return { class_settings.settings, loadsave.SaveState, true } end,
         desc   = 'Fish for one cast'
     },
     FORAGE_FARM              = {
         func   = _G.Actions.forage_farm,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Forage for ' .. item.what end
     },
     FORWARD_ZONE             = {
@@ -141,7 +142,7 @@ local task_functions = {
     },
     GENERAL_SEARCH           = {
         func   = mob.general_search,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Searching for ' .. item.npc end
     },
     GENERAL_TRAVEL           = {
@@ -151,12 +152,12 @@ local task_functions = {
     },
     GROUND_SPAWN             = {
         func   = _G.Actions.ground_spawn,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Pickup ground spawn at ' .. item.whereY .. ', ' .. item.whereX .. ', ' .. item.whereZ end
     },
     GROUND_SPAWN_FARM        = {
         func   = _G.Actions.ground_spawn_farm,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Pickup ground spawns until we obtain ' .. item.what end
     },
     GROUP_SIZE_CHECK         = {
@@ -166,7 +167,7 @@ local task_functions = {
     },
     IGNORE_MOB               = {
         func   = _G.Actions.ignore_mob,
-        params = { class_settings.settings },
+        params = function() return { class_settings.settings } end,
         desc   = function(item) return 'Add ' .. item.npc .. ' to pull ignore list' end
     },
     LDON_COUNT_CHECK         = {
@@ -181,7 +182,7 @@ local task_functions = {
     },
     LOOT                     = {
         func   = inv.loot,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Attempt to loot ' .. item.what end
     },
     NO_NAV_TRAVEL            = {
@@ -191,7 +192,7 @@ local task_functions = {
     },
     NPC_BUY                  = {
         func   = inv.npc_buy,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Purchase ' .. item.what .. ' from ' .. item.npc end
     },
     NPC_DAMAGE_UNTIL         = {
@@ -211,42 +212,42 @@ local task_functions = {
     },
     NPC_GIVE                 = {
         func   = _G.Actions.npc_give,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Give ' .. item.what .. ' to ' .. item.npc end
     },
     NPC_GIVE_ADD             = {
         func   = _G.Actions.npc_give_add,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Add ' .. item.what .. ' to give window with ' .. item.npc end
     },
     NPC_GIVE_CLICK           = {
         func   = _G.Actions.npc_give_click,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = 'Click give button'
     },
     NPC_GIVE_MONEY           = {
         func   = _G.Actions.npc_give_money,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Give money (' .. item.what .. ') to ' .. item.npc end
     },
     NPC_HAIL                 = {
         func   = _G.Actions.npc_hail,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Hail ' .. item.npc end
     },
     NPC_KILL                 = {
         func   = mob.npc_kill,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Kill ' .. item.npc end
     },
     NPC_KILL_ALL             = {
         func   = mob.npc_kill_all,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Kill all ' .. item.npc end
     },
     NPC_SEARCH               = {
         func   = mob.general_search,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Look for ' .. item.npc end
     },
     NPC_STOP_FOLLOW          = {
@@ -256,7 +257,7 @@ local task_functions = {
     },
     NPC_TALK                 = {
         func   = _G.Actions.npc_talk,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Say ' .. item.phrase .. ' to ' .. item.npc end
     },
     NPC_TALK_ALL             = {
@@ -266,22 +267,22 @@ local task_functions = {
     },
     NPC_TRAVEL               = {
         func   = travel.npc_travel,
-        params = { class_settings.settings, false, loadsave.SaveState },
+        params = function() return { class_settings.settings, false, loadsave.SaveState } end,
         desc   = function(item) return 'Move to ' .. item.npc end
     },
     NPC_TRAVEL_NO_PATH_CHECK = {
         func   = travel.npc_travel,
-        params = { class_settings.settings, true, loadsave.SaveState },
+        params = function() return { class_settings.settings, true, loadsave.SaveState } end,
         desc   = function(item) return 'Move to ' .. item.npc .. ' (ignore path)' end
     },
     NPC_WAIT                 = {
         func   = _G.Actions.npc_wait,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Wait for ' .. item.npc .. ' to spawn' end
     },
     NPC_WAIT_DESPAWN         = {
         func   = _G.Actions.npc_wait_despawn,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Wait for ' .. item.npc .. ' to despawn' end
     },
     OPEN_DOOR                = {
@@ -301,7 +302,7 @@ local task_functions = {
     },
     PH_SEARCH                = {
         func   = mob.ph_search,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Search for PH ' .. item.npc end
     },
     PICK_DOOR                = {
@@ -326,7 +327,7 @@ local task_functions = {
     },
     PRE_FARM_CHECK           = {
         func   = _G.Actions.pre_farm_check,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = 'Check if we have the items to skip the next steps'
     },
     RELOCATE                 = {
@@ -371,12 +372,12 @@ local task_functions = {
     },
     UNIGNORE_MOB             = {
         func   = _G.Actions.unignore_mob,
-        params = { class_settings.settings },
+        params = function() return { class_settings.settings } end,
         desc   = function(item) return 'Remove ' .. item.npc .. ' from pull ignore list.' end
     },
     WAIT                     = {
         func   = _G.Actions.wait,
-        params = { class_settings.settings, loadsave.SaveState },
+        params = function() return { class_settings.settings, loadsave.SaveState } end,
         desc   = function(item) return 'Wait for ' .. item.wait / 1000 .. ' seconds' end
     },
     WAIT_EVENT               = {
