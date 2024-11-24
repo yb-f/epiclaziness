3e179bfc03624f4f77bc629aeaadfc370a74abb2
Lisie <lc.x@tutanota.com>
2024-11-23 22:09:53 -0600

    feat: Version bump to v0.4.18

--- init.lua
+++ init.lua
@@ -32,7 +32,7 @@ local sqlite3 = PackageMan.Require("lsqlite3")
 --local ssl = PackageMan.Require("luasec", "ssl")
 
 --local version_url = "https://raw.githubusercontent.com/yb-f/EL-Ver/master/latest_ver"
-local version = v("0.4.15")
+local version = v("0.4.18")
 local window_flags = bit32.bor(ImGuiWindowFlags.None)
 local openGUI, drawGUI = true, true
 local myName = mq.TLO.Me.DisplayName()
